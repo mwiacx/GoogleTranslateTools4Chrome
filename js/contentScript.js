409b5d62a896b2ff8e866eeae59c5ca808665897
mwiacx <759046501@qq.com>
2018-12-16 16:34:23 +0800

    fix 'et al.' issue

--- js/contentScript.js
+++ js/contentScript.js
@@ -1,30 +1,9 @@
 function removeNewLine(){
-/*
-   var script = '' +
-        'function removeNewLine(){\n' +
-        'var textarea = document.getElementById("source");\n' +
-        //'console.log(textarea.value);' +
-        //'document.body.style.backgroundColor = "' + color + '";' +
-        'var string = textarea.value;\n' +
-        'var re = new RegExp(\'\\n\',"gm");\n' +
-        'var result = string.replace(re, "");\n' +
-        //'console.log(result);' +
-        'textarea.value = result;\n' +
-        '}\n' +
-        'var input = document.getElementById("gt-submit");\n' +
-        'let oldfunc = input.onclick;\n' +
-        'input.onclick = function(element) {\n' +
-        'removeNewLine();\n' +
-        'oldfunc(element);\n' +
-        '};';
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        chrome.tabs.executeScript(tabs[0].id, {code: script});
-    });
-*/
+
     var domain = document.domain;
     var textarea;
     if ("translate.google.cn" == domain ||
-    "translate.google.com.hk == domain")
+        "translate.google.com.hk" == domain)
         textarea = document.getElementById("source");
     else if ("fanyi.baidu.com" == domain)
         textarea = document.getElementById("baidu_translate_input");
@@ -37,32 +16,30 @@ function removeNewLine(){
 
     /* Let every sentence separated by a blank line. */
     re = new RegExp("[\.!;]\\s*");
+    reviseRe = new RegExp(".*et al$"); // 修正et al.的换行问题
     var sentences = string.split(re, 100);
-    var result = sentences[0]+"."; /* TODO: Use the original punctuation */
-    for (var i = 1; i < sentences.length-1; i++) // length-1: remove the extra blank lines.
+    var result = "";
+    for (var i = 0; i < sentences.length-1; i++) // length-1: remove the extra blank lines.
     {
-        result += "\n";
-        result += sentences[i]+"."; /* TODO: Use the original punctuation */
+        if (sentences[i].search(reviseRe) == -1)
+            result += sentences[i]+".\n"; /* TODO: Use the original punctuation */
+        else
+            result += sentences[i]+ ". ";
     }
     /* Rewrite the textarea. */
-    //console.log(result);
-    //document.getElementById("gt-submit").blur();
     textarea.value = result;
-    //textarea.focus();
 }
 
 function inject(){
     //console.log("inject success!");
     var domain = document.domain;
     //console.log(domain);
-    var button;
+    var button = null;
     if ("translate.google.cn" == domain ||
-        "translate.google.com.hk == domain"){
-        //input = document.getElementById("gt-submit");
-        //input.value = "去除换行并翻译";
+        "translate.google.com.hk" == domain){
         inject_div = document.getElementsByClassName(
             "source-footer-wrap source-or-target-footer");
-        console.log("injet_div length: " + inject_div.length);
+        //console.log("injet_div length: " + inject_div.length);
         if (inject_div.length == 1){
             button = document.createElement("button");
             button.innerText = "修正换行";
@@ -77,7 +54,8 @@ function inject(){
     * 添加第二个事件，该事件触发应该在click之前，保证先去除回车
     * 再翻译，选取mousedown事件。
     */
-    button.addEventListener("mousedown", removeNewLine);
+    if (button)
+        button.addEventListener("mousedown", removeNewLine);
     //console.log("OK?");
 }
 
